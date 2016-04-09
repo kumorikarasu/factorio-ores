24d8447a5b0f44e2f3525459fa89e4cea9fa4a49
Kumori <kumori@ryougi.com>
2016-04-09 04:48:28 -0400

    reduces gravel spawn chance

--- prototypes/chalcopyrite.lua
+++ prototypes/chalcopyrite.lua
@@ -3,5 +3,5 @@
 data.raw["resource"]["copper-ore"].minable.result = nil
 data.raw["resource"]["copper-ore"].minable.results = {
     {type="item", name="copper-ore", amount_min=1, amount_max=1, probability=1},
-    {type="item", name="gravel", amount_min=1, amount_max=1, probability=0.4}
+    {type="item", name="gravel", amount_min=1, amount_max=1, probability=0.2}
 }
--- prototypes/hematite.lua
+++ prototypes/hematite.lua
@@ -3,5 +3,5 @@
 data.raw["resource"]["iron-ore"].minable.result = nil
 data.raw["resource"]["iron-ore"].minable.results = {
   {type="item", name="iron-ore", amount=1},
-  {type="item", name="gravel", amount_min=1, amount_max=1, probability=0.4},
+  {type="item", name="gravel", amount_min=1, amount_max=1, probability=0.2},
 }
